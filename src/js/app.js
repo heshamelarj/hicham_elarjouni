2df6364020e626eb652550c0f53e392bd51077b7
heshamelarj <hicham.elarg@gmail.com>
2018-06-12 02:38:27 +0000

    responsive menu animations and styling tweaks + padiing styling for blog and work section + styling the contact form

--- src/js/app.js
+++ src/js/app.js
@@ -2,29 +2,27 @@
 document.querySelector('#menu__fullscreen').classList.add('hidden-responsive-menu');
 
 
-let menuIcon = document.querySelector('.menu');
+var menuIcon = document.querySelector('.menu');
 menuIcon.addEventListener('click',function(e){
 e.preventDefault();
-
-document.querySelector('#menu__fullscreen').classList.add('shown-responsive-menu');
 document.querySelector('#menu__fullscreen').classList.remove('hidden-responsive-menu');
+document.querySelector('#menu__fullscreen').classList.add('shown-responsive-menu');
 document.querySelector('#full-container').classList.add('hidden-full-container');
-document.querySelector('#full-container').classList.remove('shown-full-container');
 document.querySelector('footer').classList.add('hidden-full-container');    
-
+//remove the body margin-bottom 
+document.querySelector('body').classList.add('remove-margin-bottom');
 
 }) ;
 
-let closeMenuIcon = document.querySelector('#closeFullscreenMenu');
+var closeMenuIcon = document.querySelector('#closeFullscreenMenu');
 closeMenuIcon.addEventListener('click',function(e){
   e.preventDefault();
-  document.querySelector('#menu__fullscreen').classList.add('hidden-responsive-menu');
   document.querySelector('#menu__fullscreen').classList.remove('shown-responsive-menu');
-  document.querySelector('#full-container').classList.add('shown-full-container');
+  document.querySelector('#menu__fullscreen').classList.add('hidden-responsive-menu');
   document.querySelector('#full-container').classList.remove('hidden-full-container');
   document.querySelector('footer').classList.remove('hidden-full-container');
-  document.querySelector('footer').classList.add('shown-full-container');
-
+  //Add back the body margin-bottom
+  document.querySelector('body').classList.remove('remove-margin-bottom');
 });
 //when click on links
 
